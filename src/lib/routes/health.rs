c4752273a913f526762f9e249b5761a8f1c3a9d5
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-11-10 14:02:28 -0800

    improvement: change health check response to JSON

--- src/lib/routes/health.rs
+++ src/lib/routes/health.rs
@@ -1,9 +1,20 @@
 // src/lib/handlers/health.rs
 
 // dependencies
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::Serialize;
+
+// struct type to represent the /health_check endpoint response
+#[derive(Serialize)]
+struct HealthCheckResponse {
+    status: &'static str,
+}
 
 // health check handler; returns 200 OK and an empty body
 pub async fn health_check() -> impl IntoResponse {
-    StatusCode::OK
+    let response = HealthCheckResponse {
+        status: "ok"
+    }; 
+    
+   (StatusCode::OK, Json(response))
 }
