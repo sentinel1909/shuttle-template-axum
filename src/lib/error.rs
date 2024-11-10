fb4c42146ec6113fa3b661023bf873383326d7a6
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-11-09 19:20:41 -0800

    improvement: tweak error handling crate

--- src/lib/error.rs
+++ src/lib/error.rs
@@ -1,4 +1,4 @@
-// src/lib/errors.rs
+// src/lib/error.rs
 
 // dependencies
 use axum::http::StatusCode;
@@ -19,12 +19,12 @@ pub enum ApiError {
 // implement the IntoResponse trait for the ApiError type
 impl IntoResponse for ApiError {
     fn into_response(self) -> Response {
-        let (status, msg) = match self {
-            ApiError::BadRequest(_) => (StatusCode::BAD_REQUEST, "Bad Request"),
-            ApiError::Internal(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error"),
-            ApiError::NotFound(_) => (StatusCode::NOT_FOUND, "Not Found"),
+        let (status, msg) = match &self {
+            ApiError::BadRequest(err) => (StatusCode::BAD_REQUEST, format!("Bad Request: {}", err)),
+            ApiError::Internal(err) => (StatusCode::INTERNAL_SERVER_ERROR, format!("Internal Server Error: {}", err)),
+            ApiError::NotFound(err) => (StatusCode::NOT_FOUND, format!("Not Found: {}", err)),
         };
-
+        tracing::error!("Error occurred: {:?}", self);
         (status, msg).into_response()
     }
 }
